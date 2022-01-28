a80f9d71a0fa999ed089827ee3e6cf5f0d7c59dd
ShahYashDilip <yashshah3010@gmail.com>
2022-01-28 12:13:46 +0530

    2801.1

--- demo.py
+++ demo.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import random
+
+def generate(num = 100):
+    dict = {}
+    for i in range(num):
+        contact = random.randint(1000000000, 9999999999)
+        contacts = {}
+        for j in range(100):
+            contacts[j] = []
+        for j in range(num):
+            number = random.randint(1000000000, 9999999999)
+            contacts[number // 100000000].append(number)
+        dict[contact] = contacts
+    return dict
+
+print(generate())
