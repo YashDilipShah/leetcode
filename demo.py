ffe53025767d5bcfbfac8dd1bd50dacbaeb0c84a
ShahYashDilip <yashshah3010@gmail.com>
2022-02-01 11:07:58 +0530

    0102.1

--- demo.py
+++ demo.py
@@ -1,16 +1,3 @@
-import random
-
-def generate(num = 100):
-    dict = {}
-    for i in range(num):
-        contact = random.randint(1000000000, 9999999999)
-        contacts = {}
-        for j in range(100):
-            contacts[j] = []
-        for j in range(num):
-            number = random.randint(1000000000, 9999999999)
-            contacts[number // 100000000].append(number)
-        dict[contact] = contacts
-    return dict
-
-print(generate())
+boxTypes = [[5,10],[2,5],[4,7],[3,9]]
+array = sorted(boxTypes, key = lambda x : x[1], reverse=True)
+print(array)
